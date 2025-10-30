929d39b3c863a3b522a705fc75d191f81dd3d052
nati-tewolde <natisnewemail@gmail.com>
2025-10-30 11:12:15 -0400

    Add House subclass

---
+++ src/main/java/com/pluralsight/classes/House.java
@@ -0,0 +1,19 @@
+package com.pluralsight.classes;
+
+public class House extends FixedAsset {
+    private int yearBuilt;
+    private int squareFeet;
+    private int bedrooms;
+
+    public House(String name, double value, int yearBuilt, int squareFeet, int bedrooms) {
+        super(name, value);
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+
+    @Override
+    public double getValue() {
+        return 100.0 * squareFeet;
+    }
+}
